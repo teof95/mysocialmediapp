ca933890fe2dce2970a69f7997ec7cbd0750863a
Alex Kan <teof95@gmail.com>
2020-12-05 19:04:38 +0100

    added authentication token

--- routes/users.js
+++ routes/users.js
@@ -6,11 +6,25 @@ const bcryptjs = require ('bcryptjs');
 const gravatar = require("gravatar");
 const config = require("config");
 const jwt  = require("jsonwebtoken");
+const authentication = require('../middleware/authentication');
+
+
+router.get("/", authentication, async(req,res) => {
+    try {
+        let user = await User.findById(req.user.id).select("-password");
+        res.json(user);        
+    } catch (error) {
+    console.error (error.message);
+    return res.status (500).send("server error");
+    }    
+})
 
 
 
 //test if the route works (for postman test)
 // router.get('/', (req,res) => res.send ("Hello"));
+
+// ----------------------->register user<-----------------------------
 
 router.post('/register', 
 [
@@ -28,14 +42,15 @@ try {
     let fetchedUserNameFromDatabase = await User.findOne({userName}).select("-password");
     let errors = validationResult(req);
 
-    if(!errors.isEmpty()) return res.status(400).json({errors: errors.array()});
+    if(!errors.isEmpty()) 
+    return res.status(400).json({errors: errors.array()});
     
 //need to update this
     if(user) 
     return res.status(401).send("There is already a user registered with this email");
     
-    if(fetchedUserNameFromDatabase) 
-    return res.status(401).send("There is already a user registered with this username");
+    if(fetchedUserNameFromDatabase === userName) 
+    return res.status(401).json("There is already a user registered with this username");
 
 
 
@@ -43,7 +58,7 @@ try {
         r: 'pg',
         d: 'mm',
         s: '200'
-    })
+    });
 
     let newUser = new User({
         name,
@@ -53,11 +68,13 @@ try {
         password,
         avatar,
     });
+
     const salt = await bcryptjs.genSalt(10);
 
     let hashedPassword = await bcryptjs.hash(password,salt);
 
     newUser.password = hashedPassword;
+    
     await newUser.save();
 
     const payload = {
@@ -68,8 +85,9 @@ try {
 
     jwt.sign(
         payload,
-        config.get("jsonWebTokenSecret"), {expiresIn: 3600}, (err, token) =>
-        {
+        config.get("jsonWebTokenSecret"), 
+        {expiresIn: 3600}, 
+        (err, token) => {
             if (err) throw err;
             res.json({ token });
         }
@@ -78,17 +96,65 @@ try {
 
 
 } catch (error) {
-    console.log (error.message);
+    console.error (error.message);
     return res.status (500)
     .send("server error");
 }
 });
 
-// router.post('/login', async (req, res)=>{
+// ----------------------->Login user<-----------------------------
+
+router.post('/login', 
+[
+    check("email",'E-mail is empty').isEmail(),
+    check("password",'Password should be 6-12 characters').isLength({min: 6, max: 12 }),
+
+],
+async (req, res)=>{
+try {
+    let {email, password} = req.body;
+    let user = await User.findOne({email});
+    let errors = validationResult(req);
+
+    if(!errors.isEmpty()) 
+    return res.status(400).json({errors: errors.array()});
+    
+//need to update this
+    if(!user) 
+    return res.status(404)
+    .send("user with this email has not been created yet");
+    
+
+    let doPasswordsMatch = await bcryptjs.compare(password, user.password);
+
+    if(!doPasswordsMatch) 
+    return res.status(401).json("Passwords do not match");
 
 
-// });
+    const payload = {
+        user: {
+            id: user._id,
+        },
+    };
 
+    jwt.sign(
+        payload,
+        config.get("jsonWebTokenSecret"), 
+        {expiresIn: 3600}, 
+        (err, token) => {
+            if (err) throw err;
+            res.json({ token });
+        }
+    );
+
+
+
+} catch (error) {
+    console.error (error.message);
+    return res.status (500)
+    .send("server error");
+}
+});
 
 
 module.exports = router;
