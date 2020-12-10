df1f396c452b103553ffb884fcd53ef31c30e109
Alex Kan <teof95@gmail.com>
2020-12-10 16:29:09 +0100

    changed structure on connectdb

--- config/connectToDatabase.js
+++ config/connectToDatabase.js
@@ -1,18 +1,39 @@
 const mongoose = require("mongoose");
 const config = require("config");
+// const { error } = require("console");
 
-const connectToDatabase = async () => {
-  try {
-    await mongoose.connect(config.get("mongoURI"), {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-      useCreateIndex: true,
-    });
-    console.log("MongoDb is connected...");
-  } catch (error) {
-    console.error(error);
-    process.exit(1);
-  }
-};
+
+const connectToDatabase = (mongoURI) =>
+mongoose
+.connect (mongoURI,{
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+        useCreateIndex: true,
+      })
+      .then ((err) => {
+        console.log(
+          `Connected to Mongodb! database name: "${x.connections[0].name}"`
+        );
+      })
+      .catch((err) =>{
+        console.error("error connecting to mongo",err);
+      });
+
+
+
+// const connectToDatabase = async () => {
+//   try {
+//     await mongoose.connect(config.get("mongoURI"), {
+//       useNewUrlParser: true,
+//       useUnifiedTopology: true,
+//       useCreateIndex: true,
+//     });
+//     console.log("MongoDb is connected...");
+//   } catch (error) {
+//     console.error(error);
+//     process.exit(1);
+//   }
+// };
+
 
 module.exports = connectToDatabase;
--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+require("dotenv").config()
 const express = require('express');
 const app = express();
 const connectToDatabase = require('./config/connectToDatabase');
@@ -5,9 +6,11 @@ const cors = require("cors");
 const http = require('http');
 
 
+const {MONGO_ATLAS, MONGO_LOCAL, NODE_ENV } = process.env;
 
-//function to connect to express app to database
-connectToDatabase();
+
+NODE_ENV === "development" ? connectToDatabase(MONGO_LOCAL) : connectToDatabase(MONGO_ATLAS);
+
 
 //prevent cors policy warning
 app.use(cors());
@@ -63,3 +66,5 @@ let PORT = process.env.PORT || 5000;
 server.listen(PORT, () => 
 console.log(`server is on port: ${PORT}`)
 );
+
+
